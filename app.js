8e9a12820f0644a50f140feda679fbacf84a19cb
shibukl321 <io0907op@gmail.com>
2025-09-04 15:57:33 +0900

    Delete app.js

--- app.js
+++
@@ -1,3 +0,0 @@
-async function fetchSession(){try{return await (await fetch(window.BACKEND_BASE+'/session',{credentials:'include'})).json()}catch(e){return null}}
-function applySessionUI(s){if(document.getElementById('adminArea')){document.getElementById('adminArea').style.display=s?.isAdmin?'block':'none'}}
-(async()=>{const s=await fetchSession();applySessionUI(s);})();
